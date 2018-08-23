33a1a3e3097c676af45c9137fef258c6d15b5d95
Jisang Hong <wona23@gmail.com>
2018-08-24 01:15:55 +0900

    menu modified (responsive)

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ InlineEditor
         console.error(error);
     });
 });
+
+// Login
+$(document).ready(function() {
+    $('.ui.form').form({
+        fields: {
+            email: {
+                identifier: 'email',
+                rules: [{
+                        type: 'empty',
+                        prompt: '이메일을 입력하세요.'
+                    },
+                    {
+                        type: 'email',
+                        prompt: '올바른 이메일 형식을 입력하세요.'
+                    }
+                ]
+            },
+            password: {
+                identifier: 'password',
+                rules: [{
+                        type: 'empty',
+                        prompt: '비밀번호를 입력하세요.'
+                    },
+                    {
+                        type: 'length[6]',
+                        prompt: '비밀번호는 적어도 6글자 이상이어야 합니다.'
+                    }
+                ]
+            }
+        }
+    });
+});
